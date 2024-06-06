06d01a107b57b5c4c9dd82e7dff0445ea20a74c9
David J. Felix <felix.davidj@gmail.com>
2024-06-06 00:09:04 -0500

    feat: add an assumerole policy

--- packages/infr-aws-base/src/auth/iam/k8s.ts
+++ packages/infr-aws-base/src/auth/iam/k8s.ts
@@ -1,15 +1,27 @@
 import {iam} from '@pulumi/aws'
+import * as pulumi from '@pulumi/pulumi'
+import {accountId} from '../../config'
+
+export const mainClusterAdminAssumeRolePolicy = pulumi.output(accountId).apply(
+  (accountId) =>
+    iam.getPolicyDocumentOutput({
+      statements: [
+        {
+          actions: ['sts:AssumeRole'],
+          effect: 'Allow',
+          principals: [
+            {
+              identifiers: [accountId],
+              type: 'AWS',
+            },
+          ],
+        },
+      ],
+    }).json,
+)
 
 export const mainClusterAdminRole = new iam.Role('main-cluster-admin', {
-  assumeRolePolicy: iam.getPolicyDocumentOutput({
-    statements: [
-      {
-        actions: ['sts:AssumeRole'],
-        effect: 'Allow',
-        principals: [],
-      },
-    ],
-  }).json,
+  assumeRolePolicy: mainClusterAdminAssumeRolePolicy,
 })
 
 export const mainClusterAdminPolicy = new iam.Policy('main-cluster-admin', {
--- packages/infr-aws-base/src/index.ts
+++ packages/infr-aws-base/src/index.ts
@@ -1,3 +1,4 @@
+export * from './auth'
 export * from './k8s'
 export * from './net'
 export * from './outputs'
