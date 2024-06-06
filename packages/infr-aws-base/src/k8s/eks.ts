d47255b12d54fe2b51a3375c1afa5c44b2a0d40c
David J. Felix <felix.davidj@gmail.com>
2024-06-06 00:18:07 -0500

    feat: get rid of the account hardcoding

--- packages/infr-aws-base/src/k8s/eks.ts
+++ packages/infr-aws-base/src/k8s/eks.ts
@@ -1,26 +1,36 @@
 import * as eks from '@pulumi/eks'
+import * as pulumi from '@pulumi/pulumi'
 import {mainClusterAdminRole} from '../auth/iam/k8s'
-import {tags} from '../config'
+import {callerArn, tags} from '../config'
 import {vpc} from '../net/vpc'
 
 const {vpcId, privateSubnetIds, publicSubnetIds} = vpc
 
 // Create an EKS cluster with the default configuration.
-export const cluster = new eks.Cluster('cluster', {
-  vpcId,
-  privateSubnetIds,
-  publicSubnetIds,
-  roleMappings: [
-    {
-      groups: ['system:masters'],
-      roleArn: mainClusterAdminRole.arn,
-      username: 'main-cluster-admin',
-    },
-  ],
-  userMappings: [
-  ],
-  tags,
-})
+export const cluster = pulumi.output(callerArn).apply(
+  (callerArn) =>
+    new eks.Cluster('cluster', {
+      vpcId,
+      privateSubnetIds,
+      publicSubnetIds,
+      roleMappings: [
+        {
+          groups: ['system:masters'],
+          roleArn: mainClusterAdminRole.arn,
+          username: 'main-cluster-admin',
+        },
+      ],
+      userMappings: [
+        // TODO: remove this and use the role instead
+        {
+          groups: ['system:masters'],
+          userArn: callerArn,
+          username: 'manual-admin',
+        },
+      ],
+      tags,
+    }),
+)
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig
